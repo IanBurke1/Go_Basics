9e12c2c112fc636562173303b986d9ebffd63d0b
Ian Burke <iano.burke@gmail.com>
2018-09-25 12:19:35 +0100

    Understanding variables

---
+++ 02_variables.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// MAIN TYPES:
+	// string
+	// bool
+	// int
+	// int  int8  int16  int32  int64 -
+	// uint uint8 uint16 uint32 uint64 uintptr - unsigned int is 0+.. no negative numbers
+	// byte - alias for uint8
+	// rune - alias for int32
+	// float32 float64
+	// complex64 complex128 - for really large numbers
+
+	//using var
+	//var name = "Ian"
+	var age = 24
+	const isCool = true //const = constant
+	var size float32 = 2.3
+
+	//Shorthand
+	name := "Ian"
+
+	fmt.Println(name, age, isCool, size)
+	//get the type
+	fmt.Printf("%T\n", age)
+}
+
+// To run the program enter: go run main.go
+// To create an executable file enter: go build
+// The exe file will be stored in the bin folder.
