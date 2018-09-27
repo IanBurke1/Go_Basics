daf55fd5c1f0b744e4254ec731b30e759daaeace
Ian Burke <iano.burke@gmail.com>
2018-09-27 11:10:36 +0100

    Closures

---
+++ 11_closures.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// adder func with anonymous func with no name
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func main() {
+	sum := adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(sum(i))
+	}
+}
